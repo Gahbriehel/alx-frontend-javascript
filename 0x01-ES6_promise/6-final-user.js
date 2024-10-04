429c90152715655527328a773c13383ca3e59ead
gahbriehel <babatise002@gmail.com>
2024-10-04 02:55:37 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  signUpUser(firstName, lastName);
-  uploadPhoto(fileName);
-  return Promise.all([signUpUser(), uploadPhoto()]).then((value) => [
-    {
-      status: Promise.status(),
-      value: Promise.resolve ? value : Error,
-    },
-  ]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) => res.map((item) => ({
+    status: item.status,
+    value: item.status === 'fulfilled' ? item.value : String(item.reason),
+  })));
 }
