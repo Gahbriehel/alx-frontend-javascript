ed6118bf374c9e4a25d61fb79ca567ac401480f3
gahbriehel <babatise002@gmail.com>
2024-10-09 04:01:08 +0100

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,9 +4,11 @@ export default function updateStudentGradeByCity(arr, city, newGrades) {
     .map((item) => {
       for (const grade of newGrades) {
         if (grade.studentId === item.id) {
-          item.grade = grade.grade;
+          const mapItem = item;
+          mapItem.grade = grade.grade;
         } else {
-          item.grade = "N/A";
+          const mapItem = item;
+          mapItem.grade = 'N/A';
         }
       }
       return item;
