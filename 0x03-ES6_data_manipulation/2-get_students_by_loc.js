7763ea61110aaed551465bf4572f34f367272419
gahbriehel <babatise002@gmail.com>
2024-10-09 03:16:23 +0100

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,3 @@
 export default function getStudentsByLocation(arr, city) {
   return arr.filter((e) => e.location === city);
 }
-
-// myArray.filter((element, index, array) => { /* ... */ })
-// let filteredUsers = users.filter((user) => {
-//    return user.age > 40 && user.occupation === 'programmer';
-// });
